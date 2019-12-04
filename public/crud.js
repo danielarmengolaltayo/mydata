edaaa3c108031c8086dd80af5cce4696a5696418
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2019-12-04 17:48:34 +0100

    spaces

--- public/crud.js
+++ public/crud.js
@@ -5,57 +5,57 @@ module.exports = {
     //////////// CREATE
 
     createEnt: async function (Ent, self) {
-        try{ 
+        try {
             let ent = new Ent({ self: self });
             ent = await ent.save();
             return ent;
-        }catch(err){ console.log("ERROR createEnt: " + err); }
+        } catch (err) { console.log("ERROR createEnt: " + err); }
     },
     createInt: async function (Int, a, b, c) {
-        try{ 
+        try {
             let int = new Int({ a: a, b: b, c: c });
             int = await int.save();
             return int._id;
-        }catch(err){ console.log("ERROR createInt: " + err); }
+        } catch (err) { console.log("ERROR createInt: " + err); }
     },
     createRel: async function (Rel, idInt) {
-        try{ 
+        try {
             let rel = new Rel();
             rel.list.push(idInt);
             rel = await rel.save();
             return rel._id;
-        }catch(err){ console.log("ERROR createRel: " + err); }
+        } catch (err) { console.log("ERROR createRel: " + err); }
     },
 
     //////////// READ
 
     findEntBySelf: async function (Ent, s) {
-        try{
+        try {
             let foundEnt = await Ent.findOne({ self: s }, function (err, foundEnt) {
                 if (!err) { return foundEnt; }
             });
             return foundEnt;
-        }catch(err){ console.log("ERROR findEntBySelf: " + err); }
+        } catch (err) { console.log("ERROR findEntBySelf: " + err); }
     },
 
     //////////// UPDATE
 
     updateEnt: async function (Ent, idEnt, idRel) {
-        try{
+        try {
             let foundEnt = await Ent.findOne({ _id: idEnt }, function (err, foundEnt) {
                 if (!err) { return foundEnt; }
             });
             foundEnt.others.push(idRel);
             await foundEnt.save();
-        }catch(err){ console.log("ERROR updateEnt: " + err); }
+        } catch (err) { console.log("ERROR updateEnt: " + err); }
     },
     updateRel: async function (Rel, idRel, idInt) {
-        try{
+        try {
             let foundRel = await Rel.findOne({ _id: idRel }, function (err, foundRel) {
                 if (!err) { return foundRel; }
             });
             foundRel.list.push(idInt);
             await foundRel.save();
-        }catch(err){ console.log("ERROR updateRel: " + err); }
+        } catch (err) { console.log("ERROR updateRel: " + err); }
     }
 };
